0ea46325505d7e36dfca70f070254ecba031dd0d
Muneeb <muneebansari280608@gmail.com>
2026-05-09 18:46:21 +0500

    Write code in Landingpage, Model , Login , Input , Signup .tsx files video time: 41:41

--- frontend/interview-prep-ai/src/pages/Auth/Login.tsx
+++ frontend/interview-prep-ai/src/pages/Auth/Login.tsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Input from "../../components/Inputs/input";
 
-const Login = () => {
+const Login = ({ setCurrentPage }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  // Handle Login Form Submit
+  const handleLogin = async (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>Login</div>
-  )
-}
+    <div className="w-full p-8 flex flex-col justify-center">
+      <h3 className="text-lg font-semibold text-black">Welcome Back</h3>
+      <p className="text-xs text-slate-700 mt-[5px] mb-6">Please enter your details to log in</p>
 
-export default Login
+      <form onSubmit={handleLogin}>
+        <Input
+          value={email}
+          onChange={({ target }) => setEmail(target.value)}
+          label="Email Address"
+          placeholder="john@example.com"
+          type="text"
+        />
+
+        <Input
+          value={password}
+          onChange={({ target }) => setPassword(target.value)}
+          label="Password"
+          placeholder="Min 8 Characters"
+          type="password"
+        />
+        {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+
+        <button
+          type="submit"
+          className="mt-2 w-full rounded-lg bg-black py-2.5 text-sm font-semibold text-white transition-colors hover:bg-red-100 hover:text-black"
+        >
+          LOGIN
+        </button>
+
+        <p className="text-[13px] text-slate-800 mt-3">
+          Don't have an account?{" "}
+          <button
+            className="font-medium text-primary underline cursor-pointer"
+            onClick={() => {
+              setCurrentPage("signup");
+            }}
+          >
+            SignUp
+          </button>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
--- frontend/interview-prep-ai/src/pages/Auth/SignUp.tsx
+++ frontend/interview-prep-ai/src/pages/Auth/SignUp.tsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Input from "../../components/Inputs/input";
 
-const SignUp = () => {
-  return (
-    <div>SignUp</div>
-  )
-}
+const SignUp = ({ setCurrentPage }) => {
+	const [profilePic, setProfilePic] = useState(null);
+	const [fullName, setFullName] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
 
-export default SignUp
+	const [error, setError] = useState(null);
+
+	const navigate = useNavigate();
+
+	// Handle SignUp Form Submit
+	const handleSignUp = async (e) => {
+		e.preventDefault();
+	};
+
+	return (
+		<div className="w-full p-8 flex flex-col justify-center">
+			<h3 className="text-lg font-semibold text-black">Create an Account</h3>
+			<p className="text-xs text-slate-700 mt-[5px] mb-6">
+				Join us today by entering your details below.
+			</p>
+
+			<form onSubmit={handleSignUp}>
+				<Input
+					value={fullName}
+					onChange={({ target }) => setFullName(target.value)}
+					label="Full Name"
+					placeholder="John"
+					type="text"
+				/>
+
+				<Input
+					value={email}
+					onChange={({ target }) => setEmail(target.value)}
+					label="Email Address"
+					placeholder="john@example.com"
+					type="text"
+				/>
+
+				<Input
+					value={password}
+					onChange={({ target }) => setPassword(target.value)}
+					label="Password"
+					placeholder="Min 8 Characters"
+					type="password"
+				/>
+
+				{error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+
+				<button
+					type="submit"
+					className="mt-2 w-full rounded-lg bg-black py-2.5 text-sm font-semibold text-white transition-colors hover:bg-red-100 hover:text-black"
+				>
+					SIGN UP
+				</button>
+
+				<p className="text-[13px] text-slate-800 mt-3">
+					Already an account?{" "}
+					<button
+						className="font-medium text-primary underline cursor-pointer"
+						onClick={() => {
+							setCurrentPage("login");
+						}}
+					>
+						Login
+					</button>
+				</p>
+			</form>
+		</div>
+	);
+};
+
+export default SignUp;
